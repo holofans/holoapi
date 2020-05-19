110b5d26960b94561249243605828e0538f1629d
dragonjet <jetriconew@gmail.com>
2020-05-19 02:03:16 -0700

    db migrations update

--- database/migrations/20200516040233_initial.js
+++ database/migrations/20200516040233_initial.js
@@ -1,64 +1,65 @@
 
 exports.up = (knex) => knex.schema
   .createTable('Channel', (tbl) => {
-    tbl.string('vtuberKey', 30).primary();
-    tbl.integer('bbSpaceId').unique().nullable();
-    tbl.integer('bbRoomId').unique().nullable();
-    tbl.string('ytChannelId', 24).unique().nullable();
-    tbl.string('ytUploadsId', 24).nullable();
+    tbl.bigincrements('id').primary();
+    tbl.string('yt_channel_ref', 24).unique().nullable();
+    tbl.string('yt_uploads_ref', 24).unique().nullable();
+    tbl.integer('bb_space_ref').unique().nullable();
+    tbl.integer('bb_room_ref').unique().nullable();
     tbl.text('name').notNullable();
     tbl.text('description').nullable();
     tbl.string('thumbnail', 150).nullable();
-    tbl.datetime('publishedAt').nullable();
-    tbl.text('linkTwitter').nullable();
-    tbl.text('linkTwitch').nullable();
-    tbl.text('linkFacebook').nullable();
-    tbl.text('linkInstagram').nullable();
-    tbl.datetime('createdOn').notNullable();
-    tbl.datetime('updatedOn').nullable();
-    tbl.datetime('crawledOn').nullable();
+    tbl.datetime('published_at').nullable();
+    tbl.string('status', 20).notNullable().defaultTo('new');
+    tbl.text('link_twitter').nullable();
+    tbl.text('link_twitch').nullable();
+    tbl.text('link_facebook').nullable();
+    tbl.text('link_instagram').nullable();
+    tbl.datetime('created_on').notNullable();
+    tbl.datetime('updated_on').nullable();
+    tbl.datetime('crawled_on').nullable();
   })
   .createTable('ChannelStats', (tbl) => {
-    tbl.string('statsKey', 50).primary();
-    tbl.string('vtuberKey', 30);
-    tbl.integer('bbFollowers').notNullable();
-    tbl.integer('ytViews').nullable();
-    tbl.integer('ytSubscribers').notNullable();
-    tbl.datetime('createdOn').notNullable();
-    tbl.datetime('updatedOn').nullable();
+    tbl.bigincrements('id').primary();
+    tbl.bigInteger('channel_id').notNullable();
+    tbl.integer('bb_followers').notNullable();
+    tbl.integer('yt_views').nullable();
+    tbl.integer('yt_subscribers').notNullable();
+    tbl.datetime('created_on').notNullable();
+    tbl.index(['channel_id', 'created_on'], 'stats_key');
   })
   .createTable('Video', (tbl) => {
-    tbl.string('videoKey', 14).primary();
-    tbl.string('vtuberKey', 30);
-    tbl.integer('bbVideoId').nullable();
-    tbl.string('ytVideoId', 11).nullable();
+    tbl.bigincrements('id').primary();
+    tbl.bigInteger('channel_id').notNullable();
+    tbl.integer('bb_video_ref').unique().nullable();
+    tbl.string('yt_video_ref', 11).unique().nullable();
     tbl.text('title').nullable();
     tbl.text('description').nullable();
     tbl.string('thumbnail', 150).nullable();
-    tbl.datetime('publishedAt').nullable();
+    tbl.datetime('published_at').nullable();
     tbl.string('status', 20).notNullable().defaultTo('new');
-    tbl.datetime('liveSchedule').nullable();
-    tbl.datetime('liveStart').nullable();
-    tbl.datetime('liveEnd').nullable();
-    tbl.integer('liveViewers').nullable();
-    tbl.boolean('isUploaded').nullable();
-    tbl.integer('lateSecs').nullable();
+    tbl.datetime('live_schedule').nullable();
+    tbl.datetime('live_start').nullable();
+    tbl.datetime('live_end').nullable();
+    tbl.integer('live_viewers').nullable();
+    tbl.boolean('is_uploaded').nullable();
+    tbl.integer('late_secs').nullable();
     tbl.integer('duration').nullable();
-    tbl.boolean('isCaptioned').nullable();
-    tbl.boolean('isLicensed').nullable();
-    tbl.boolean('isEmbeddable').nullable();
-    tbl.datetime('createdOn').notNullable();
-    tbl.datetime('updatedOn').nullable();
-    tbl.datetime('crawledOn').nullable();
+    tbl.boolean('is_captioned').nullable();
+    tbl.boolean('is_licensed').nullable();
+    tbl.boolean('is_embeddable').nullable();
+    tbl.datetime('created_on').notNullable();
+    tbl.datetime('updated_on').nullable();
+    tbl.datetime('crawled_on').nullable();
   })
   .createTable('VideoComment', (tbl) => {
-    tbl.string('commentKey', 50).primary();
-    tbl.string('videoKey', 14).notNullable();
-    tbl.string('ytCommentId', 29).nullable();
+    tbl.bigincrements('id').primary();
+    tbl.bigInteger('video_id').notNullable();
+    tbl.string('yt_comment_ref', 29).unique().nullable();
     tbl.integer('stamp').notNullable();
     tbl.string('message').notNullable();
-    tbl.datetime('createdOn').notNullable();
-    tbl.datetime('updatedOn').nullable();
+    tbl.datetime('created_on').notNullable();
+    tbl.datetime('updated_on').nullable();
   });
 
 exports.down = (knex) => knex.schema
