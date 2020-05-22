20840ebd118a808b5ee8f2a1580d3fb743fa074a
Alexander <sashok935@gmail.com>
2020-05-22 20:37:16 +0300

    Move error handlers to middlewares instead of routes

--- apps/client-api/index.js
+++ apps/client-api/index.js
@@ -5,23 +5,25 @@ const express = require('express');
 const helmet = require('helmet');
 const moment = require('moment-timezone');
 
-const routesSystem = require('./routes/system');
-const routesError = require('./routes/error');
-// const routesV1 = require('./routes/v1');
-
 const { log } = require('../../modules');
+const { notFoundHandler, errorHandler } = require('./middleware/error');
+// const routesV1 = require('./routes/v1');
+const routesSystem = require('./routes/system');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 2324;
+
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ strict: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(routesSystem);
 // app.use('/v1', routesV1);
-app.use(routesError);
 
-app.listen(PORT, async () => {
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
   log.info('HOLOAPI | %s | %s | :%d', process.env.NODE_ENV, moment().format('YYYY-MM-DD HH:mm:ss ZZ'), PORT);
 });
