5b68bf1707af0615d9ae6109a383e249b3c6da20
dragonjet <jetriconew@gmail.com>
2020-06-03 23:43:40 -0700

    crawler-youtube: channelInfo re-refactor

--- apps/crawler-youtube/tasks/channel-info.js
+++ apps/crawler-youtube/tasks/channel-info.js
@@ -1,18 +1,19 @@
 /**
- * UPDATE CHANNEL INFOS
- * Update information daily
+ * CHANNEL INFO
+ * Update all channel information daily
  */
 
 require('dotenv').config();
 const moment = require('moment-timezone');
 const { Op } = require('sequelize');
-const { db, youtube, log } = require('../../../modules');
+// eslint-disable-next-line object-curly-newline
+const { db, youtube, log, GenericError } = require('../../../modules');
 
 module.exports = async () => {
   log.debug('channelInfo() START');
 
   // Fetch channels that needs to be updated, and get their keys
-  const channelKeys = (await db.Channel.findAll({
+  const channelInstances = (await db.Channel.findAll({
     where: {
       [Op.and]: [
         { yt_channel_id: { [Op.not]: null } },
@@ -24,10 +25,16 @@ module.exports = async () => {
         },
       ],
     },
-  })).map((v) => v.yt_channel_id);
+  }));
 
   // Check if there's any channel to be updated
-  if (!channelKeys || !channelKeys.length) return Promise.resolve({ skip: true });
+  if (!channelInstances || !channelInstances.length) {
+    log.debug('channelInfo() No channels to be updated');
+    return Promise.resolve({ skip: true });
+  }
+
+  // Get YouTube channel keys/ids
+  const channelKeys = channelInstances.map((v) => v.yt_channel_id);
 
   // Get channels by page
   const batch = [];
@@ -35,27 +42,82 @@ module.exports = async () => {
   // For each 50 keys, split, then join into string
   while (channelKeys.length) batch.push(channelKeys.splice(0, 50).join(','));
 
-  // Function to transform each keys into youtube results
-  const search = async (items) => (await youtube.channels.list({
-    part: 'snippet,contentDetails',
-    id: items,
-    maxResults: 50,
-  })).data.items;
+  // Convert batch array into promises that fetch youTube data
+  const ytFetches = batch.map(async (batchItems) => (
+    // Fetch data from YouTube
+    youtube.channels.list({
+      part: 'snippet,contentDetails',
+      id: batchItems,
+      maxResults: 50,
+    })
+      .then((ytResult) => {
+        // Sanity check for YouTube respons contents
+        if (!ytResult.data || !ytResult.data.items) {
+          return Promise.reject(new GenericError('YouTube response error', ytResult));
+        }
+        // Return the array of channel information within the response
+        return ytResult.data.items;
+      })
+      .catch((err) => {
+        // Log error information, and the channels included in the API that failed
+        log.error('channelInfo() YouTube fetch error', {
+          error: err.toString(),
+          batchItems,
+        });
+        return [];
+      })
+  ));
 
-  // Apply function
-  const results = (await Promise.all(batch.map(search))).reduce((c, v) => c.concat(v), []);
+  // Wait for all YouTube responses
+  const channelInfos = (await Promise.all(ytFetches)).reduce((c, v) => c.concat(v), []);
 
-  // Process each result from function
-  const updateResults = await Promise.all(results.map((channelInfo) => db.Channel.upsert({
-    yt_channel_id: channelInfo.id,
-    yt_uploads_id: channelInfo.contentDetails.relatedPlaylists.uploads,
-    name: channelInfo.snippet.title,
-    description: channelInfo.snippet.description,
-    thumbnail: channelInfo.snippet.thumbnails.high.url,
-    published_at: moment(channelInfo.snippet.publishedAt).tz('UTC'),
-    updated_at: moment.utc(),
-  })));
+  // Check if all channels were returned
+  if (channelInfos.length !== channelInstances.length) {
+    // Get YouTube IDs of channels returned by the API
+    const returnedKeys = channelInfos.map((channelInfo) => channelInfo.id);
+    // Warn about the missing channels
+    log.warn('channelInfo() Not all requested channels were returned by YouTube', {
+      missing: channelInstances
+        .map((channel) => channel.yt_channel_id)
+        .filter((channelKey) => returnedKeys.indexOf(channelKey) === -1),
+    });
+    // Possibly notify admins about the "missing" / deleted channel
+    // ...
+  }
 
-  log.info('channelInfo() Saved channel information', { results: updateResults });
-  return Promise.resolve();
+  // Record results for all channel saves
+  const logResults = {};
+
+  // Convert channel data into promises that save into database
+  const dbSaves = channelInfos.map((channelInfo) => (
+    // Update databse record, insert if any unique key does not exist yet
+    db.Channel.upsert({
+      yt_channel_id: channelInfo.id,
+      yt_uploads_id: channelInfo.contentDetails.relatedPlaylists.uploads,
+      name: channelInfo.snippet.title,
+      description: channelInfo.snippet.description,
+      thumbnail: channelInfo.snippet.thumbnails.high.url,
+      published_at: moment(channelInfo.snippet.publishedAt).tz('UTC'),
+      updated_at: moment.utc(),
+    })
+      .then((dbResult) => {
+        // Add to result list
+        logResults[channelInfo.id] = dbResult;
+      })
+      .catch((err) => {
+        // Log error
+        log.error('channelInfo() Cannot save to database', {
+          channelInfo,
+          error: err.toString(),
+        });
+        // Add to result list
+        logResults[channelInfo.id] = null;
+      })
+  ));
+
+  // Wait for all database saves
+  await Promise.all(dbSaves);
+
+  log.info('channelInfo() Saved channel information', { results: logResults });
+  return Promise.resolve({ done: true });
 };
---
+++ modules/error.js
@@ -0,0 +1,15 @@
+
+class GenericError extends Error {
+  constructor(message, data) {
+    super(message);
+    this.data = data;
+  }
+
+  toString() {
+    return `${super.toString()} | ${JSON.stringify(this.data)}`;
+  }
+}
+
+module.exports = {
+  GenericError,
+};
