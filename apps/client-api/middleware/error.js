b8dda263b5025a5a2a29cda17ef7dadf2d01d2e3
Alexander <sashok935@gmail.com>
2020-06-15 22:40:29 +0300

    Add route async middleware and proper error responses

--- apps/client-api/middleware/error.js
+++ apps/client-api/middleware/error.js
@@ -1,24 +1,25 @@
-const { logger } = require('../../../modules');
+const { GenericError, log } = require('../../../modules');
 
 module.exports = {
   notFoundHandler(req, res) {
-    res.status(404).json({ error: 404 });
+    res.status(404).json({ message: 'Not found.' });
   },
 
   // eslint-disable-next-line no-unused-vars
   errorHandler(err, req, res, next) {
-    // const customError = err instanceof CustomAppError;
-    // const statusCode = customError ? 400 : 500;
-    const statusCode = 500;
-    logger.error(`Request body: ${JSON.stringify(req.body)}`);
-    logger.error(`Original error: ${err}`);
-    logger.error(`Stack trace: ${err.stack}`);
+    const customError = err instanceof GenericError;
+    const statusCode = customError ? 400 : 500;
+    log.error(`Request body: ${JSON.stringify(req.body)}`);
+    log.error(`Stack trace: ${err.stack}`);
 
-
-    const errorObj = {
+    const errorObject = {
       message: err.message || 'Unhandled error!',
     };
 
-    res.status(statusCode).json(errorObj);
+    res.status(statusCode).json(errorObject);
+  },
+
+  asyncMiddleware: (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
   },
 };
