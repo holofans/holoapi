6db6e41339643f9f08874c4bcef5a832781c2ac0
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-06-19 00:16:47 +0800

    video-info-api: minor linting

--- apps/crawler-youtube/tasks/video-info-api.js
+++ apps/crawler-youtube/tasks/video-info-api.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-inline-comments */
 /**
  * VIDEO INFO API
  * Fetches information of 50 most outdated videos and updates database
@@ -11,177 +12,252 @@ require('dotenv').config();
 const moment = require('moment-timezone');
 const { Op } = require('sequelize');
 const { db, youtube, log, GenericError } = require('../../../modules');
-const consts = require('../../../consts');
+const { STATUSES: { NEW, LIVE, PAST, UPCOMING, MISSING } } = require('../../../consts');
 
 const VIDEOS_MAX_QUERY = 50;
 
 module.exports = async () => {
+  log.debug('videoInfoAPI() START');
   try {
-    log.debug('videoInfoAPI() START');
 
     // Get times
     const utcDate = moment.tz('UTC');
 
     // Check if there are videos set as status [new]
     const newVideos = await db.Video.findAll({
+
       where: [
+
         { yt_video_key: { [Op.not]: null } },
-        { status: consts.STATUSES.NEW },
+        { status: NEW },
+
       ],
+
       limit: VIDEOS_MAX_QUERY,
-    }).catch((err) => {
+
+
+    }).catch(({ message: error }) => {
+
       // Catch and log db error
-      log.error('videoInfoAPI() Unable to fetch videos with status[new]', { error: err.toString() });
+      log.error('videoInfoAPI() Unable to fetch videos with status[new]', { error });
+
       // Return empty list, so the succeeding process for outdated videos wil continue
       return [];
+
     });
 
+
     // Find non-new videos that are most outdated
-    // const outdatedVideos = await db.Video.findAll({
-    //   where: {
-    //     [Op.and]: [
-    //       { yt_video_key: { [Op.not]: null } },
-    //       { status: { [Op.ne]: consts.STATUSES.NEW } },
-    //       { updated_at: { [Op.lt]: moment.tz('Asia/Tokyo').startOf('day') } },
-    //     ],
-    //   },
-    //   order: [
-    //     ['updated_at', 'ASC'],
-    //   ],
-    //   limit: VIDEOS_MAX_QUERY - newVideos.length,
-    // }).catch((err) => {
-    //   // Catch and log db error
-    //   log.error('videoInfoAPI() Unable to fetch outdated videos', { error: err.toString() });
-    //   // Return empty list, so the new videos from the preceeding process can still be updated
-    //   return [];
-    // });
-    const outdatedVideos = [];
+    const outdatedVideos = await db.Video.findAll({
+
+      where: {
+
+        [Op.and]: [
+
+          { yt_video_key: { [Op.not]: null } },
+          { status: { [Op.ne]: NEW } },
+          { updated_at: { [Op.lt]: moment.tz('Asia/Tokyo').startOf('day') } }
+
+        ]
+      },
+
+      order: [
+
+        ['updated_at', 'ASC']
+
+      ],
+
+      limit: VIDEOS_MAX_QUERY - newVideos.length
+
+
+    }).catch(({ message: error }) => {
+
+      // Catch and log db error
+      log.error('videoInfoAPI() Unable to fetch outdated videos', { error });
+
+      // Return empty list, so the new videos from the preceeding process can still be updated
+      return [];
+    });
 
     // Final list of videos to be updated
     const targetVideos = newVideos.concat(outdatedVideos);
 
+
     // Check if there's any channel to be crawled
     if (!targetVideos || !targetVideos.length) {
-      log.debug('videoInfoAPI() No videos to be updated');
-      return;
+
+      return log.debug('videoInfoAPI() No videos to be updated');
+
     }
 
     // Fetch data from YouTube
     const ytVideoItems = await youtube.videos.list({
+
       part: 'snippet,status,contentDetails,liveStreamingDetails',
-      id: targetVideos.map((targetVideo) => targetVideo.yt_video_key),
+      id: targetVideos.map(({ yt_video_key }) => yt_video_key),
       hl: 'ja',
       fields: 'items(id,snippet,contentDetails,status/embeddable,liveStreamingDetails)',
       maxResults: 50, // keep at 50, not VIDEOS_MAX_QUERY
+
     })
+
       .then((ytResult) => {
+
         // Sanity check for YouTube respons contents
         if (!ytResult.data || !ytResult.data.items) {
+
           return Promise.reject(new GenericError('Invalid YouTube response', ytResult));
+
         }
+
         // Return video list only
         return ytResult.data.items;
       })
-      .catch((err) => {
+
+      .catch(({ message: error }) => {
+
         // Log error information
         log.error('videoInfoAPI() YouTube fetch error', {
-          error: err.toString(),
-          videoKeys: targetVideos.map((targetVideo) => targetVideo.yt_video_key),
+
+          error,
+          videoKeys: targetVideos.map(({ yt_video_key }) => yt_video_key),
+
         });
+
+
         // Return so that loop will not continue, and this call has no items returned
         return null;
+
       });
+
 
     // Check if we have videos to be updated in database
     if (!ytVideoItems) {
-      log.warn('videoInfoAPI() No videos fetched');
-      return;
+
+      return log.warn('videoInfoAPI() No videos fetched');
+
     }
 
-    // Record results for all video saves
-    const logResults = {};
 
-    // Save using the model instances
-    const dbSaves = targetVideos.map((targetVideo) => {
-      // Check if this target video was returned by YouTube API
-      const ytInfo = ytVideoItems.find((ytVideoItem) => ytVideoItem.id === targetVideo.yt_video_key);
-      let saveInfo;
-      if (ytInfo) {
-        // Video still exists, update its information
-        saveInfo = {
-          title: ytInfo.snippet.title,
-          description: ytInfo.snippet.description,
-          published_at: moment(ytInfo.snippet.publishedAt),
-          is_captioned: String(ytInfo.contentDetails.caption) !== 'false',
-          is_licensed: ytInfo.contentDetails.licensedContent,
-          is_embeddable: ytInfo.status.embeddable || null,
-        };
-        // Video livestream status
-        if (ytInfo.liveStreamingDetails) {
-          saveInfo.is_uploaded = false;
-          saveInfo.live_schedule = ytInfo.liveStreamingDetails.scheduledStartTime || null;
-          saveInfo.live_start = ytInfo.liveStreamingDetails.actualStartTime || null;
-          saveInfo.live_end = ytInfo.liveStreamingDetails.actualEndTime || null;
-          saveInfo.live_viewers = ytInfo.liveStreamingDetails.concurrentViewers || null;
-          // Get moment objects
-          const scheduleMoment = moment(saveInfo.live_schedule);
-          const startMoment = moment(saveInfo.live_start);
-          const endMoment = moment(saveInfo.live_end);
-          // Determine live status
-          if (saveInfo.live_end) {
-            saveInfo.status = consts.STATUSES.PAST;
-          } else if (saveInfo.live_start) {
-            saveInfo.status = consts.STATUSES.LIVE;
-          } else if (saveInfo.live_schedule) {
-            if (moment().isSameOrAfter(scheduleMoment)) {
-              saveInfo.status = consts.STATUSES.LIVE;
-            } else {
-              saveInfo.status = consts.STATUSES.UPCOMING;
-            }
-          } else {
-            saveInfo.status = consts.STATUSES.PAST;
-          }
-          // Get derived values
-          if (saveInfo.live_schedule && saveInfo.live_start) {
-            saveInfo.late_secs = parseInt(startMoment.diff(scheduleMoment, 'seconds'), 10);
-          }
-          if (saveInfo.live_end && saveInfo.live_start) {
-            saveInfo.duration_secs = parseInt(endMoment.diff(startMoment, 'seconds'), 10);
-          }
-        } else {
-          // Not a live stream, an uploaded video
-          saveInfo.is_uploaded = true;
-          saveInfo.status = consts.STATUSES.PAST;
-          saveInfo.duration_secs = parseInt(moment.duration(ytInfo.contentDetails.duration).as('seconds'), 10);
-        }
-      } else {
-        // Video not returned by YouTube, mark as missing
-        saveInfo = {
-          status: consts.STATUSES.MISSING,
+    // Wait for all database saves and record results
+    const logResults = (await Promise.all(
+
+      // Save using the model instances
+      targetVideos.map(targetVideo => {
+
+        // Check if this target video was returned by YouTube API + Destrucutre
+        const ytInfo = ytVideoItems.find(({ id }) => id === targetVideo.yt_video_key);
+        const {
+
+          snippet: { title, description, publishedAt },
+          liveStreamingDetails,
+          contentDetails: { caption, licensedContent: is_licensed },
+          status: { embeddable },
+
+        } = ytInfo || { snippet: [], contentDetails: [], status: [] };
+
+        const {
+
+          scheduledStartTime,
+          actualStartTime,
+          actualEndTime,
+          concurrentViewers,
+
+        } = liveStreamingDetails || {};
+
+
+        const saveInfo = !ytInfo ? {
+
+          // Video still exists, update its information
+          title,
+          description,
+          published_at: moment(publishedAt),
+          is_captioned: String(caption) !== 'false',
+          is_licensed,
+          is_embeddable: embeddable || null,
+
+          // Video livestream status
+          is_uploaded: !liveStreamingDetails,
+
+        } : {
+
+          // Video not returned by YouTube, mark as missing
+          status: MISSING,
           updated_at: utcDate,
-        };
-      }
-      return targetVideo.update(saveInfo)
-        .then(() => {
-          // Add to result list
-          logResults[targetVideo.yt_video_key] = true;
-        })
-        .catch((err) => {
-          // Log error
-          log.error('videoInfoAPI() Cannot save to database', {
-            videoKey: targetVideo.yt_video_key,
-            error: err.toString(),
-          });
-          // Add to result list
-          logResults[targetVideo.yt_video_key] = false;
-        });
-    });
 
-    // Wait for all database saves
-    await Promise.all(dbSaves);
+        };
+
+        if (liveStreamingDetails) {
+
+          Object.assign(saveInfo, {
+
+            // Video livestream status
+            live_schedule: scheduledStartTime || null,
+            live_start: actualStartTime || null,
+            live_end: actualEndTime || null,
+            live_viewers: concurrentViewers || null,
+
+
+            /* eslint-disable */ 
+            // Determine live status
+            status:   !actualStartTime   ?   UPCOMING
+                  :   !actualEndTime     ?   LIVE
+                  :                          PAST,
+
+          },
+
+          // Get derived values
+          actualStartTime && scheduledStartTime && {
+
+            late_secs:
+
+            (+new Date(actualStartTime) - +new Date(scheduledStartTime) / 1000) | 0,
+
+          },
+
+          actualEndTime && actualStartTime && {
+
+            duration_secs:
+
+            (+new Date(actualEndTime) - +new Date(actualStartTime) / 1000) | 0,
+
+          });
+        } /* eslint-enable */
+
+
+        return targetVideo.update(saveInfo)
+
+          .then(() => {
+
+            // Add to result list
+            return { [targetVideo.yt_video_key]: true };
+
+          })
+
+          .catch(({ message: error }) => {
+
+            // Log error
+            log.error('videoInfoAPI() Cannot save to database', {
+
+              videoKey: targetVideo.yt_video_key,
+              error
+
+            });
+
+            // Add to result list
+            return { [targetVideo.yt_video_key]: false };
+
+          });
+
+      }),
+    )).filter(v => v).flat();
+
 
     log.info('videoInfoAPI() Saved video list', { results: logResults });
-  } catch (error) {
-    log.error('videoInfoAPI() Uncaught error', { error: error.toString() });
+
+  } catch ({ message: error }) {
+
+    log.error('videoInfoAPI() Uncaught error', { error });
+
   }
 };
