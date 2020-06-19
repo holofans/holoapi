8bb72bed56433f2cff58576038b7d4e2df788bee
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2020-06-19 03:11:06 -0700

    const fix

--- database/migrations/20200618211042-crawl-channels.js
+++ database/migrations/20200618211042-crawl-channels.js
@@ -1,22 +1,22 @@
-const consts = require('../../consts');
+const { TABLES } = require('../../consts');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn(consts.TABLE_VIDEO, 'comments_crawled_at');
-    await queryInterface.addColumn(consts.TABLE_CHANNEL, 'comments_crawled_at', { type: Sequelize.DataTypes.DATE });
-    await queryInterface.removeColumn(consts.TABLE_VIDEO_COMMENT, 'timecode');
-    await queryInterface.addColumn(consts.TABLE_VIDEO_COMMENT, 'comment_key',
+    await queryInterface.removeColumn(TABLES.TABLE_VIDEO, 'comments_crawled_at');
+    await queryInterface.addColumn(TABLES.TABLE_CHANNEL, 'comments_crawled_at', { type: Sequelize.DataTypes.DATE });
+    await queryInterface.removeColumn(TABLES.TABLE_VIDEO_COMMENT, 'timecode');
+    await queryInterface.addColumn(TABLES.TABLE_VIDEO_COMMENT, 'comment_key',
       { type: Sequelize.DataTypes.STRING, allowNull: false, unique: true });
-    await queryInterface.changeColumn(consts.TABLE_VIDEO_COMMENT, 'message', { type: Sequelize.DataTypes.TEXT });
-    await queryInterface.removeIndex(consts.TABLE_VIDEO_COMMENT, ['message']);
+    await queryInterface.changeColumn(TABLES.TABLE_VIDEO_COMMENT, 'message', { type: Sequelize.DataTypes.TEXT });
+    await queryInterface.removeIndex(TABLES.TABLE_VIDEO_COMMENT, ['message']);
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn(consts.TABLE_VIDEO, 'comments_crawled_at', { type: Sequelize.DataTypes.DATE });
-    await queryInterface.removeColumn(consts.TABLE_CHANNEL, 'comments_crawled_at');
-    await queryInterface.addColumn(consts.TABLE_VIDEO_COMMENT, 'timecode', { type: Sequelize.DataTypes.INTEGER });
-    await queryInterface.removeColumn(consts.TABLE_VIDEO_COMMENT, 'comment_key');
-    await queryInterface.changeColumn(consts.TABLE_VIDEO_COMMENT, 'message', { type: Sequelize.DataTypes.STRING });
-    await queryInterface.addIndex(consts.TABLE_VIDEO_COMMENT, ['message']);
+    await queryInterface.addColumn(TABLES.TABLE_VIDEO, 'comments_crawled_at', { type: Sequelize.DataTypes.DATE });
+    await queryInterface.removeColumn(TABLES.TABLE_CHANNEL, 'comments_crawled_at');
+    await queryInterface.addColumn(TABLES.TABLE_VIDEO_COMMENT, 'timecode', { type: Sequelize.DataTypes.INTEGER });
+    await queryInterface.removeColumn(TABLES.TABLE_VIDEO_COMMENT, 'comment_key');
+    await queryInterface.changeColumn(TABLES.TABLE_VIDEO_COMMENT, 'message', { type: Sequelize.DataTypes.STRING });
+    await queryInterface.addIndex(TABLES.TABLE_VIDEO_COMMENT, ['message']);
   },
 };
