cd3eee4785907786d0a6d7ef6135c7c33471abd4
dragonjet <jetriconew@gmail.com>
2020-06-19 04:20:09 -0700

    .env.example update, no more heartbeat

--- apps/crawler-youtube/index.js
+++ apps/crawler-youtube/index.js
@@ -9,7 +9,6 @@ const videoListFeed = require('./tasks/video-list-feed');
 // const videoListScrape = require('./tasks/video-list-scrape')
 const videoInfoAPI = require('./tasks/video-info-api');
 const videoStatusAPI = require('./tasks/video-status-api');
-// const videoStatusHeart = require('./tasks/video-status-heart')
 const comments = require('./tasks/comments');
 
 const { env } = process;
@@ -35,11 +34,6 @@ schedule.scheduleJob('video-info-api', env.SCHEDULE_VIDEO_INFO_API, 'Asia/Tokyo'
 
 // Checks status of known live videos using YouTube Data API
 schedule.scheduleJob('video-status-api', env.SCHEDULE_VIDEO_STAT_API, 'Asia/Tokyo', videoStatusAPI);
-
-// Checks status of known live videos using heartbeat
-// schedule.scheduleJob('video-status-heart', config.timings['video-status-heart'], 'Asia/Tokyo', function(){
-//   videoStatusHeart()
-// })
 
 // Gets comments from videos to check for timestamps
 schedule.scheduleJob('video-comments', env.SCHEDULE_COMMENTS, 'Asia/Tokyo', comments);
