7edcb8c0e3d4fded6b2768b66bd4c8b62c2b078d
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2020-06-21 16:59:25 -0700

    what if we split it into multiple queries

--- apps/client-api/routes/v1/comments.js
+++ apps/client-api/routes/v1/comments.js
@@ -1,12 +1,44 @@
-const { Op } = require('sequelize');
+const sequelize = require('sequelize');
 const { Router } = require('express');
 const { fixchar } = require('fixchar');
-const { RESPONSE_FIELDS } = require('../../../../consts');
+const { RESPONSE_FIELDS, CACHE_TTL } = require('../../../../consts');
 const { db, GenericError } = require('../../../../modules');
 const { asyncMiddleware } = require('../../middleware/error');
 const { limitChecker } = require('../../middleware/filters');
+const { channel, comments } = require('../../../../database/models/video');
+const cacheService = require('../../services/CacheService');
+
+const { Op } = sequelize;
 
 const router = new Router();
+
+const getTotalCount = async (q, channel_id) => {
+  const cacheKey = `count:${q}_${channel_id}`;
+  const cache = await cacheService.getFromCache(cacheKey);
+  if (cache.cached) {
+    return cache;
+  }
+
+  // object structure: { count: 132 }
+  const count = await db.VideoComment.findOne({
+    attributes: [sequelize.fn('count', sequelize.fn('distinct', sequelize.col('video_id')))],
+    where: { message: { [Op.iLike]: `%${q}%` } },
+    subQuery: false,
+    raw: true,
+    ...channel_id && {
+      include: [
+        {
+          association: 'video',
+          attributes: [],
+          where: { channel_id },
+        },
+      ],
+    },
+  });
+
+  cacheService.saveToCache(cacheKey, count, CACHE_TTL.COMMENTS);
+  return count;
+};
 
 router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
   const { limit = 25, offset = 0, channel_id, q } = req.query;
@@ -18,33 +50,46 @@ router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
   // Sanitizing query to remove full width alphanumeric and half-width kana
   const sanitizedQuery = fixchar(q).trim();
 
-  const { rows, count } = await db.Video.findAndCountAll({
-    attributes: RESPONSE_FIELDS.VIDEO,
+  const totalCount = getTotalCount(sanitizedQuery, channel_id);
+
+  const videoIds = await db.VideoComment.findAll({
+    attributes: ['video.id'],
     include: [
       {
-        association: 'comments',
-        attributes: RESPONSE_FIELDS.VIDEO_COMMENT_SIMPLE,
-        where: { message: { [Op.iLike]: `%${sanitizedQuery}%` } },
+        association: 'video',
+        attributes: [],
       },
-      ...(channel_id ? [{
-        association: 'channel',
-        attributes: RESPONSE_FIELDS.VIDEO_COMMENT_SIMPLE,
-        where: { id: channel_id },
-      }] : []),
     ],
+    where: { message: { [Op.iLike]: `%${sanitizedQuery}%` } },
     limit,
     offset,
-    // Fixes dupes counting
-    distinct: true,
+    group: 'video.id',
+    order: [[db.VideoComment.associations.video, 'published_at', 'DESC']],
     // Fixes weird subquery that kills performance
     subQuery: false,
+    raw: true,
+  })
+    .map(({ id }) => id);
+
+  const videos = await db.Video.findAll({
+    attributes: RESPONSE_FIELDS.VIDEO,
+    where: {
+      id: videoIds,
+    },
+    include: [{
+      association: comments,
+      attributes: RESPONSE_FIELDS.VIDEO_COMMENT_SIMPLE,
+    }, {
+      association: channel,
+      attributes: RESPONSE_FIELDS.CHANNEL,
+    }],
   });
 
   const results = {
-    count: rows.length,
-    total: count,
+    // count: rows.length,
+    total: (await totalCount).count,
     query: sanitizedQuery,
-    comments: rows,
+    comments: videos,
   };
 
   res.json(results);
