1dc2ef3d4d38c8fd77f1d536ba2541e10a60e64c
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2020-06-20 18:10:13 -0700

    Option 3 implemented

--- apps/client-api/routes/v1/comments.js
+++ apps/client-api/routes/v1/comments.js
@@ -2,12 +2,12 @@ const sequelize = require('sequelize');
 const { Router } = require('express');
 const { fixchar } = require('fixchar');
 const { db } = require('../../../../modules');
-const { RESPONSE_FIELDS, CACHE_TTL } = require('../../../../consts');
+const { CACHE_TTL } = require('../../../../consts');
 const { asyncMiddleware } = require('../../middleware/error');
 const { limitChecker } = require('../../middleware/filters');
 const cacheService = require('../../services/CacheService');
 
-const { Op } = sequelize;
+const { QueryTypes, Op } = sequelize;
 const router = new Router();
 
 router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
@@ -17,6 +17,7 @@ router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
 
   // sanitizing query to remove full width alphanumeric and half-width kana.
   const sanitizedQuery = fixchar(q).trim();
+  const sanitizedWildcardQuery = `%${sanitizedQuery}%`;
 
   const cacheKey = `com?q=${sanitizedQuery},cid=${channel_id},l=${limit},o=${offset}`;
   const cache = await cacheService.getFromCache(cacheKey);
@@ -24,39 +25,94 @@ router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
     return res.json(cache);
   }
 
-  const dbQuery = {
-    include: [
-      {
-        association: 'comments',
-        attributes: RESPONSE_FIELDS.VIDEO_COMMENT_SIMPLE,
-        where: {
-          message: {
-            [Op.iLike]: `%${sanitizedQuery}%`,
-          },
-        },
-      },
-      {
-        association: 'channel',
-        attributes: RESPONSE_FIELDS.CHANNEL,
-      },
-    ],
-    order: [['published_at', 'desc']],
-    // rejectOnEmpty: true,
-    ...channel_id && { where: { channel_id } },
-  };
+  const rows = db.client.query(`
+  SELECT
+    "Video".*,
+    "CommentBag".*,
+    "channel"."id" AS "channel.id",
+    "channel"."yt_channel_id" AS "channel.yt_channel_id",
+    "channel"."bb_space_id" AS "channel.bb_space_id",
+    "channel"."name" AS "channel.name",
+    "channel"."description" AS "channel.description",
+    "channel"."photo" AS "channel.photo",
+    "channel"."published_at" AS "channel.published_at",
+    "channel"."twitter_link" AS "channel.twitter_link"
+  from
+    video AS "Video"
+    JOIN (
+      SELECT
+          "Video".id,
+          array_agg(array["Comment"."comment_key", "Comment"."message"]) AS "comments"
+
+      FROM
+          (
+              SELECT
+                  "Video"."id",
+                  "Video"."yt_video_key",
+                  "Video"."bb_video_id",
+                  "Video"."title",
+                  "Video"."thumbnail",
+                  "Video"."published_at",
+                  "Video"."status",
+                  "Video"."live_schedule",
+                  "Video"."live_start",
+                  "Video"."live_end",
+                  "Video"."is_uploaded",
+                  "Video"."duration_secs",
+                  "Video"."is_captioned",
+                  "Video"."channel_id"
+              FROM
+                  "public"."video" AS "Video"
+              ORDER BY
+                  "Video"."published_at" DESC
+          ) AS "Video"
+          INNER JOIN "public"."video_comment" AS "Comment" ON "Video"."id" = "Comment"."video_id"
+          AND "Comment"."message" ILIKE $queryStr
+          ${channel_id ? ' AND "Video"."channel_id" = $channel_id ' : ''}
+      GROUP BY
+          "Video".id
+      ORDER BY
+          max("Video"."published_at") DESC
+      LIMIT
+          $queryLimit OFFSET $queryOffset
+  ) AS "CommentBag" ON "CommentBag".id = "Video".id
+  LEFT OUTER JOIN "public"."channel" AS "channel" ON "Video"."channel_id" = "channel"."id";`, {
+
+    // If plain is true, then sequelize will only return the first
+    // record of the result set. In case of false it will return all records.
+    plain: false,
+    // Set this to true if you don't have a model definition for your query.
+    raw: true,
+    // If true, transforms objects with . separated property names into nested objects using dottie.js.
+    nest: true,
+    // The type of query you are executing.
+    type: QueryTypes.SELECT,
+    bind: {
+      queryStr: sanitizedWildcardQuery,
+      channel_id,
+      queryLimit: limit,
+      queryOffset: offset,
+    },
+  });
 
   // const count = db.Video.count(dbQuery);
-  const { count, rows } = await db.Video.findAndCountAll({
-    subQuery: false,
-    attributes: RESPONSE_FIELDS.VIDEO,
-    limit,
-    offset,
-    ...dbQuery,
+  const count = db.VideoComment.count({
+    distinct: true,
+    col: 'video_id',
+    where: { message: { [Op.iLike]: sanitizedWildcardQuery } },
+    ...channel_id && {
+      include: [
+        {
+          association: 'video',
+          where: { channel_id },
+        },
+      ],
+    },
   });
 
   const results = {
-    data: rows,
-    count,
+    data: await rows,
+    count: await count,
     cached: false,
     query: sanitizedQuery,
   };
