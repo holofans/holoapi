ef44aaa547c4c9e7c2f3cb82839f7240bc640465
Alexander <sashok935@gmail.com>
2020-07-02 00:08:22 +0300

    Fix offset bug, re-add comment caching

--- apps/client-api/routes/v1/comments.js
+++ apps/client-api/routes/v1/comments.js
@@ -1,13 +1,11 @@
-const sequelize = require('sequelize');
+const { Op } = require('sequelize');
 const { Router } = require('express');
 const { fixchar } = require('fixchar');
-const { RESPONSE_FIELDS } = require('../../../../consts');
+const { RESPONSE_FIELDS, CACHE_TTL } = require('../../../../consts');
 const { db, GenericError } = require('../../../../modules');
 const { asyncMiddleware } = require('../../middleware/error');
 const { limitChecker } = require('../../middleware/filters');
-const { channel, comments } = require('../../../../database/models/video');
-
-const { Op } = sequelize;
+const cacheService = require('../../services/CacheService');
 
 const router = new Router();
 
@@ -57,24 +55,30 @@ router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
   if (!q || q.length < 1) {
     throw new GenericError('Expected ?q param');
   }
-
   // Sanitizing query to remove full width alphanumeric and half-width kana
   const sanitizedQuery = fixchar(q).trim();
+
+  const cacheKey = `comments-${sanitizedQuery};offset-${offset};limit-${limit}`;
+  const cache = await cacheService.getFromCache(cacheKey);
+  if (cache.cached) {
+    return res.json(cache);
+  }
+
   const totalCount = getTotalCount(sanitizedQuery, channel_id);
   const videoIds = await getVideoIds(q, channel_id);
-  const videoIdsInPage = videoIds.slice(offset, offset + limit);
+  const videoIdsInPage = videoIds.slice(+offset, +offset + +limit);
 
   const videos = await db.Video.findAll({
     attributes: RESPONSE_FIELDS.VIDEO,
     where: { id: videoIdsInPage },
     include: [
       {
-        association: comments,
+        association: 'comments',
         attributes: RESPONSE_FIELDS.VIDEO_COMMENT,
         required: true,
       },
       {
-        association: channel,
+        association: 'channel',
         attributes: RESPONSE_FIELDS.CHANNEL,
         required: true,
       }],
@@ -85,9 +89,12 @@ router.get('/search', limitChecker, asyncMiddleware(async (req, res) => {
     total: await totalCount,
     query: sanitizedQuery,
     comments: videos,
+    cached: false,
   };
 
-  res.json(results);
+  cacheService.saveToCache(cacheKey, results, CACHE_TTL.COMMENTS);
+
+  return res.json(results);
 }));
 
 module.exports = router;
