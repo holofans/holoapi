040b79b7adb1b566e157049989778341583e177e
Alexander <sashok935@gmail.com>
2020-07-04 01:09:38 +0300

    Fix comments inner join to include only related comment
    Replace spaces in cache keys

--- apps/client-api/services/CacheService.js
+++ apps/client-api/services/CacheService.js
@@ -3,7 +3,7 @@ const { log, memcached } = require('../../../modules');
 class CacheService {
   async getFromCache(key) {
     try {
-      const cache = await memcached.get(key);
+      const cache = await memcached.get(this._replaceWhitespaces(key));
       const channelCache = cache ? JSON.parse(cache) : {};
       channelCache.cached = !!Object.keys(channelCache).length;
 
@@ -16,10 +16,14 @@ class CacheService {
 
   async saveToCache(key, data, ttl) {
     try {
-      await memcached.set(key, JSON.stringify(data), ttl);
+      await memcached.set(this._replaceWhitespaces(key), JSON.stringify(data), ttl);
     } catch (e) {
       log.error(`Error saving to cache: ${e.message}`);
     }
+  }
+
+  _replaceWhitespaces(key) {
+    return key.replace(/\s/g, '_');
   }
 }
 
