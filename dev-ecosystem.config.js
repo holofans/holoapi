fc36ae768e0d30a908fd010d8fd49a1a0a0c47b7
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2020-08-16 22:09:57 -0700

    made health check script

--- dev-ecosystem.config.js
+++ dev-ecosystem.config.js
@@ -1,18 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'client-api',
+      name: 'api',
       script: './apps/client-api/index.js',
       exec_mode: 'cluster',
       instances: 1, // add more in prod
       watch: ['./apps/client-api'], // dev only
     },
     {
-      name: 'crawler-youtube',
+      name: 'crawler',
       script: './apps/crawler-youtube/index.js',
       exec_mode: 'cluster',
       instances: 1, // only 1 in prod, important
       watch: ['./apps/crawler-youtube'], // dev only
+    },
+    {
+      name: 'health-check',
+      script: './scripts/check_alive_or_restart.sh',
+      exec_mode: 'fork',
+      interpreter: '/bin/bash',
+      cron_restart: '*/2 * * * *', // every two minutes
+      watch: false,
+      autorestart: false,
     },
   ],
 };
---
+++ scripts/check_alive_or_restart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 5 \
+    --retry-max-time 40 \
+    'http://localhost:2434/v1/live' > /dev/null
+status=$?
+
+[ $status -eq 0 ] && echo "LGTM $(date)" || echo "SERVER HEALTH BAD $(date)"
+
+if ! [ $status -eq 0 ]
+then
+  echo "RESTARTING"
+  pm2 restart api
+fi
