b5ac7ce324e9c6cdb8b3ce828c1f32d55a8d4ed1
David Chen <juandavidchen@gmail.com>
2020-09-03 18:16:13 -0700

    Update seed file to enforce id increments and allow updating existing rows

--- database/seeders/20200601074220-channels-hololive.js
+++ database/seeders/20200601074220-channels-hololive.js
@@ -2,11 +2,26 @@ const { TABLES } = require('../../consts');
 const hololive = require('./json/hololive.json');
 
 module.exports = {
-  up: (queryInterface) => queryInterface.bulkInsert(TABLES.CHANNEL, hololive.channels.map((item) => ({
-    yt_channel_id: item.youtube,
-    name: item.ytName,
-    twitter_link: item.twitter,
-  }))),
+  up: async (queryInterface, Sequelize) => {
+    // map json to index
+    const channels = hololive.channels.map((item, index) => ({
+      id: index + 1,
+      yt_channel_id: item.youtube,
+      name: item.ytName,
+      twitter_link: item.twitter,
+    }));
+
+    // bulk insert new channels
+    await queryInterface.bulkInsert(TABLES.CHANNEL, channels, { 
+      ignoreDuplicates: true,
+    });
+
+    // update channels
+    await Promise.all(channels.map((channel) => 
+      queryInterface.bulkUpdate(TABLES.CHANNEL, channel, {
+        id: channel.id
+    })));
+  },
 
   down: (queryInterface) => queryInterface.bulkDelete(TABLES.CHANNEL, {
     yt_channel_id: hololive.channels.map((item) => item.youtube),
