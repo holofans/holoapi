a734ddd3359fc9d1796efafbdcefb505c13ae8f7
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2020-10-11 21:56:48 -0700

    reorganize to version api-doc

--- apps/client-api/routes/root.js
+++ apps/client-api/routes/root.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const { asyncMiddleware } = require('../middleware/error');
-const { SWAGGER_JSON } = require('../../../consts');
 
 const router = new Router();
 
@@ -9,11 +8,7 @@ router.get('/', asyncMiddleware(async (req, res) => {
 }));
 
 router.get('/api-docs', asyncMiddleware(async (req, res) => {
-  res.sendFile('api-docs/api-doc.html', { root: __dirname });
-}));
-
-router.get('/swagger.json', asyncMiddleware(async (req, res) => {
-  res.json(SWAGGER_JSON);
+  res.redirect('/v1/api-docs/');
 }));
 
 module.exports = router;
